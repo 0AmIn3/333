1e2cba4f005568244b604a3dade421d427ee9608
Amin <107203714+0AmIn3@users.noreply.github.com>
2022-06-15 08:43:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,218 @@
+
+
+// let name = prompt("What is your name?")
+
+// name = name.toLowerCase().trim()
+// name = name.charAt(0).toUpperCase() + name.slice(1,10)
+
+// if(name === "John"){
+//     alert(`Welcome ${name}`)
+// }
+// else{
+//     alert(`You are not John!`)
+// }
+
+
+
+
+
+
+// let a = +prompt("a")
+// let method = prompt("method")
+// let b = +prompt("b")
+
+// if(method === "+") {
+//     console.log(a + b)
+// }else if(method === "-"){
+//     console.log(a - b)
+// }else if(method === "*"){
+//     console.log(a * b)
+// }else if(method === "/"){
+//     console.log(a / b)
+// }else(
+//     console.log(
+//         "Не выдумывай"
+//     )
+// )
+
+
+
+
+
+
+
+// let age = +prompt("How old are u?")
+
+// if(age <= 10){
+//     alert("Ты еще малыш.")
+// }
+// else if(age > 10 && age < 18){
+//     alert("Вы еще подросток.")
+// }
+// else if(age >= 18 ){
+//     alert("Добро пожаловать")
+// }
+// else{
+//     alert("Введите возраст")
+// }
+
+
+
+// let box = +prompt("")
+
+// box == 1 && 2 && 12 ? alert("Зима") : console.log("error") 
+// box == 3 && 4 && 5 ? alert('Весна') : console.log("error")
+// box == 6 && 7 && 8 ? alert('Лето') : console.log("error")
+// box == 9 && 10 && 11 ? alert('Осень') : console.log("error")
+
+
+//  ДВУХЗНАЧНЫЕ ЧИСЛА НЕ РАБОТАЮТ
+
+
+
+
+
+
+// <---------------------------------------- H-W -------------------------------------> 
+
+
+
+
+
+// let promptt = prompt("day(1) or month(2)")
+// let day = new Date().getDay()
+// let month = new Date().getMonth()
+
+// switch (promptt) {
+//     case "1":
+//         console.log(day) 
+//         break;
+//     case "2":
+//         console.log(month) 
+//         break;
+//     default:
+//         console.log('error') 
+// }
+
+
+
+
+
+
+
+
+
+
+// let name = "Alex"
+// let money = 10000
+// let account = 7777
+
+// let input_1 = prompt('Как вас зовут?')
+
+// if(input_1 === name){ 
+// }  
+// else{
+//     alert("Пользователь не найден, досвидули")
+// }
+
+// let input_2 = prompt('Номер счета?')
+
+
+// if(input_2 == account){
+// }
+// else{
+// }
+// if(input_1 === name && input_2 == account){
+//     alert(`Добро пожаловать ${name}!`)
+// }
+// else{
+//     alert("Пользователь не найден, досвидули")
+// }
+
+// let input_3 = prompt('Сколько обналичить?')
+
+// let aa = money - input_3
+
+// if(money >= input_3){
+//     alert("Успешно")
+//     alert(`Вы сняли ${input_3}`)
+//     alert(`На счету осталось ${aa}.`)
+// }
+// else{
+//     alert("Недостаточно средств")
+// }
+
+
+
+
+
+
+
+// let howmush =prompt('Сколько вас?')
+// if(howmush <=4 ){
+
+// }
+// else{
+//     alert("Вы не пройдете!")
+// }
+
+// let club_1 = prompt("Как вас завут?")
+
+// if(club_1.charAt(0).toUpperCase().trim() === "A"){
+
+// }else{
+//     alert("Вы не пройдете!")
+// }
+
+// let club_2 = +prompt("Ваш возраст?")
+
+// if(club_2 >=20 && club_2 <= 40){
+// }
+// else{
+//     alert("Вы не пройдете!")
+// }
+
+// let club_3 = +prompt("Сколько у вас денег?")
+
+// if(club_3 >= 100){
+//     alert(`Добро пожаловать в наш клуб ${club_1} ! Ваш возраст ${club_2} . И в кармане ${club_3}$ `)
+// }
+// else{
+//    alert("Вы не пройдете!")
+// }
+
+
+let NAME = prompt ('What is your name?')
+let YEAR=prompt('How old are you?')
+let question=prompt('How much money do you have with you?')
+let people=+prompt("How many people want to go to the club with you?")
+
+let year1='20'
+let year2='40'
+let MONEY='100'
+
+if (NAME[0] ==='b' ){
+    console.log("Welcome")
+}
+else {
+    console.log("git out")
+    
+}
+if (year2>=year1){
+    console.log('Welcome')
+}
+else{
+    console.log("git out")
+}
+if(question>=MONEY){
+    console.log('Welcome')
+}
+else{
+    console.log("git out")
+}
+if(people>=0){
+    console.log('welcome')
+}
+else{
+    console.log("git out")
+}
